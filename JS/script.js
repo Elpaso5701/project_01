94e8f582b2fcccf36273a1c92166342bccff239c
Alex <alexxx8698@gmail.com>
2022-03-05 00:33:48 +0200

    second

---
+++ JS/script.js
@@ -0,0 +1,42 @@
+"use strict";
+/*a = 15
+console.log(a);
+
+let alex = 2;
+const hi = 3; 
+alex = 7;
+const obj = {
+a: 30
+};
+obj.a = 45;
+{
+    var alan = 34;
+}
+console.log(lala);*/
+/*const obj = {
+    name: "Alex",
+    age: 45,
+    IsMarried: false
+};
+console.log(obj ['IsMarried']);*/
+////const result = +prompt('How old are you?', '0');
+//console.log( typeof(result) );
+
+/*const answear = [];
+
+answear[0] = prompt('как вас зовут', 'укажите имя');
+answear[1] = prompt('сколько вам лет', '0');
+
+document.write(answear);*/
+
+const a = 'ba';
+console.log(`https:url.com/${a}/7`);
+
+//let b = 'дела?';
+//alert(`Как, ${b}`);
+console.log(typeof(5 + '7'));
+
+let a = 7,
+    b = 2;
+    c = '7';
+console.log( a === c );
